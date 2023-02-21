4031ba4626a4ee6d802336dd24e99865c7bddc03
Noah Richard Espinoza Dohrmann <ndohrmann@linux5.cs.uchicago.edu>
2023-02-20 20:01:53 -0600

    first project commit

---
+++ README.md
@@ -0,0 +1,5 @@
+CAAM 282 Final Project Repository
+=================================
+
+This directory will house the source code, data viz notebooks, etc. for the
+final project (simulating pedestrian dynamics).
---
+++ src/cc_temp.cc
@@ -0,0 +1,27 @@
+#include<math.h>
+#include<iostream>
+#include<fstream>
+#include<typeinfo>
+#include<vector>
+#include<string>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::system;
+using std::ctype;
+using std::vector;
+using std::pair;
+
+/* File Description: 
+ * 
+ */
+
+int main(int argc, char** argv) {
+	
+	int x = 5;
+
+	cout << "This is x " << x << endl;
+	
+	return 0;
+}
